d3a4392e5e4624fceb59aa60c95e444b8e418832
Renata Argerique <renatapva@gmail.com>
2017-04-08 22:13:54 -0300

    Create saveImage function to write to the new image. For now it only writes the first three lines

--- main.c
+++ main.c
@@ -20,18 +20,18 @@ void *do_something (void *param){
 }
 
 /* Read the image */
-void readImage(char *fileName[], struct PGMImage image){
+void readImage(char *fileName[], struct PGMImage *image){
 	/* Variables to assist with the image reading */
 	FILE *input;
 	int currentValue, numRows, numColumns;
 	
-	/* Open the file (filename received as parameter) for reading */
+	/* Open the file for reading */
 	input = fopen(fileName, "r");
 	printf("\nOpening file: %s", fileName);
 	
 	/* Read the first line related to the image type */
-	fscanf(input, "%s", &image.type);
-	printf("\nImage file of type: %s", image.type);
+	fscanf(input, "%s", &image->type);
+	printf("\nImage file of type: %s", image->type);
 	
 	/* Skip line */
 	while(getc(input) != '\n');
@@ -42,21 +42,22 @@ void readImage(char *fileName[], struct PGMImage image){
 	}
 	printf("\nSkiped comment line");
 	
-	/* Backup one position */
-	fseek(input, -1, SEEK_CUR);
+	/* Backup three positions */
+	/* I literally have no idea why we have to do this */
+	fseek(input, -3, SEEK_CUR);
 	
 	/* Read number of rows, columns and max value */
-	fscanf(input, "%d %d %d", &image.numRows, &image.numColumns, &image.maxValue);
-	printf("\nNumber of rows: %d \nNumber of columns: %d \nMax brightness value: %d", image.numRows, image.numColumns, image.maxValue);
+	fscanf(input, "%d %d %d", &image->numRows, &image->numColumns, &image->maxValue);
+	printf("\nNumber of rows: %d \nNumber of columns: %d \nMax brightness value: %d", image->numRows, image->numColumns, image->maxValue);
 	
 	/* Reads each of the pixels and adds up 55 pixels to each */
 	printf("\nStart reading the pixels of the image");
-	for(numRows=image.numRows-1; numRows>=0; numRows--){
-		for(numColumns=image.numColumns-1; numColumns>=0; numColumns--){
+	for(numRows=image->numRows-1; numRows>=0; numRows--){
+		for(numColumns=image->numColumns-1; numColumns>=0; numColumns--){
 			fscanf(input, "%d", &currentValue);
 			/* Making sure we do not try to set a pixel with more than 255 */
 			if(currentValue < 200){
-				image.image[numRows][numColumns] = currentValue + 55;
+				image->image[numRows][numColumns] = currentValue + 55;
 			}
 		}
 	}
@@ -65,6 +66,29 @@ void readImage(char *fileName[], struct PGMImage image){
 	/* Close the file */
 	fclose(input);
 	printf("\nClosed the file");	
+}
+
+/* Save the image */
+void saveImage(char *fileName[], struct PGMImage *image){
+	/* Variables to assist with the aving of the image */
+	FILE *output;
+	
+	/* Open the file for writing */
+	output = fopen(fileName, "w");
+	printf("\nOpening file for writing: %s", fileName);
+	
+	/* Write the first line related to the image type */
+	fprintf(output, &image->type);
+	fprintf(output, "\n");
+	printf("\nWrote type of file", image->type);
+	
+	/* Write number of rows, columns and max value */
+	fprintf(output, "%d %d\n%d\n", image->numRows, image->numColumns, image->maxValue);
+	printf("\nWriting number of rows, columns and max value of brightness");
+	
+	/* Close the file */
+	fclose(output);
+	printf("\nClosed the new file");	
 }
 
 int main(int argc, char *argv[]) {
@@ -76,7 +100,10 @@ int main(int argc, char *argv[]) {
 	int parameter = 0;
 	
 	/* Calls method to read PGM image */
-	readImage(argv[1], image);
+	readImage(argv[1], &image);
+	
+	/* Calls method to write the new PGM image*/
+	saveImage(argv[2], &image);
 	
 	pthread_create(&thread0, NULL, do_something, parameter);
 	pthread_join(thread0, NULL);
